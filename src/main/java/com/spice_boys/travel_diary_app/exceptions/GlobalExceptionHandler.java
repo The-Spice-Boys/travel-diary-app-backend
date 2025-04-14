8de0564eb9f32e78405d21c7f5802ea251ad565b
jabc8 <jt.024@outlook.com>
2025-04-14 13:17:13 +0100

    Add error handling

---
+++ src/main/java/com/spice_boys/travel_diary_app/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.spice_boys.travel_diary_app.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> HandleNoSuchElement(NoSuchElementException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
