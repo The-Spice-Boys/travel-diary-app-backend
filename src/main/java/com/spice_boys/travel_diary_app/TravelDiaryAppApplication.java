e1f6f80728f5c0fbb3b05ed72a200c60b6b20772
jabc8 <jt.024@outlook.com>
2025-04-14 10:53:08 +0100

    Set up application

---
+++ src/main/java/com/spice_boys/travel_diary_app/TravelDiaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.spice_boys.travel_diary_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDiaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDiaryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spice_boys/travel_diary_app/TravelDiaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spice_boys.travel_diary_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDiaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
