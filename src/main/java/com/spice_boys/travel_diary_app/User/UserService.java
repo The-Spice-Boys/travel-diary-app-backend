2e2861b4347a650811ddc90a6cb017c68b17097c
jabc8 <jt.024@outlook.com>
2025-04-14 10:53:38 +0100

    Create UserService class

---
+++ src/main/java/com/spice_boys/travel_diary_app/User/UserService.java
@@ -0,0 +1,22 @@
+package com.spice_boys.travel_diary_app.User;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
